13237c2e80e5d6284bd842379ada08f5d8b09260
sajicode <sajioloye@gmail.com>
2021-06-18 00:30:23 +0100

    feat: use iframe to handle code execution

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
 	const ref = useRef<any>();
-	const [ input, setInpur ] = useState('');
-	const [ code, setCode ] = useState('');
+  const iframe = useRef<any>();
+	const [ input, setInput ] = useState('');
 
 	const startService = async () => {
 		ref.current = await esbuild.startService({
@@ -23,6 +23,8 @@ const App = () => {
 	const onClick = async () => {
 		if (!ref.current) return;
 
+    iframe.current.srcdoc = html;
+
 		// * transpile inputed code
 		const result = await ref.current.build({
 			entryPoints: [ 'index.js' ],
@@ -35,16 +37,37 @@ const App = () => {
 			}
 		});
 
-		setCode(result.outputFiles[0].text);
+		// setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
 	};
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data)
+            } catch (err) {
+              const root = document.querySelector("#root");
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false)
+        </script>
+      </body>
+    </html>
+  `;
 
 	return (
 		<div>
-			<textarea onChange={(e) => setInpur(e.target.value)} value={input} />
+			<textarea onChange={(e) => setInput(e.target.value)} value={input} />
 			<div>
 				<button onClick={onClick}>Submit</button>
 			</div>
-			<pre>{code}</pre>
+      <iframe ref={iframe} srcDoc={html} title="preview" sandbox="allow-scripts"/>
 		</div>
 	);
 };
