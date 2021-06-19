19be031c72ad43d0918acae9c66ee94e33dcb11e
sajicode <sajioloye@gmail.com>
2021-06-19 02:05:19 +0100

    feat: install & configure monaco editor - add prettier & eslint config

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -3,66 +3,70 @@ import axios from 'axios';
 import localForage from 'localforage';
 
 const fileCache = localForage.createInstance({
-	name: 'filecache'
+  name: 'filecache'
 });
 
 export const fetchPlugin = (inputCode: string) => {
-	return {
-		name: 'fetch-plugin',
-		setup(build: esbuild.PluginBuild) {
-			build.onLoad({ filter: /(^index\.js$)/ }, () => {
-				return {
-					loader: 'jsx',
-					contents: inputCode
-				};
-			});
+  return {
+    name: 'fetch-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode
+        };
+      });
 
-    build.onLoad({ filter: /.*/ }, async (args: any) =>  {
-      // check to see if we already fetched file
-				// check if in cache
-				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // check to see if we already fetched file
+        // check if in cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
 
-				// if in cache, return it immediately
-				if (cachedResult) return cachedResult;
-    })
+        // if in cache, return it immediately
+        if (cachedResult) return cachedResult;
+      });
 
-			build.onLoad({ filter: /.css$/ }, async (args: any) => {
-				const { data, request } = await axios.get(args.path);
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
 
-				// * remove new line characters, double & single quotes
-				const escaped = data.replace(/\n/g, '').replace(/"/g, '\\"').replace(/'/g, "\\'");
+        // * remove new line characters, double & single quotes
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
 
-				const contents = `
+        const contents = `
         const style = document.createElement('styel');
         style.innerText = '${escaped}';
         document.head.appendChild(style)
       `;
 
-				const result: esbuild.OnLoadResult = {
-					loader: 'jsx',
-					contents,
-					resolveDir: new URL('./', request.responseURL).pathname
-				};
-				// store response in cache
-				await fileCache.setItem(args.path, result);
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname
+        };
+        // store response in cache
+        await fileCache.setItem(args.path, result);
 
-				return result;
-			});
+        return result;
+      });
 
-			build.onLoad({ filter: /.*/ }, async (args: any) => {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
 
-				const { data, request } = await axios.get(args.path);
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
+        };
+        // store response in cache
+        await fileCache.setItem(args.path, result);
 
-				const result: esbuild.OnLoadResult = {
-					loader: 'jsx',
-					contents: data,
-					resolveDir: new URL('./', request.responseURL).pathname
-				};
-				// store response in cache
-				await fileCache.setItem(args.path, result);
-
-				return result;
-			});
-		}
-	};
+        return result;
+      });
+    }
+  };
 };
