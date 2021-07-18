f4c5fea0b5c9a6a65cd6b4b26cfc5f6ee6533710
sajicode <sajioloye@gmail.com>
2021-07-18 16:52:04 +0100

    feat: integrate immer into redux

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-case-declarations */
+import produce from 'immer';
 import { ActionType } from '../action-types.ts';
 import { Action } from '../actions';
 import { Cell } from '../cell';
@@ -18,35 +20,53 @@ const initialState: CellsState = {
   data: {}
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
+const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
-      // eslint-disable-next-line no-case-declarations
       const { id, content } = action.payload;
 
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [action.payload.id]: {
-            ...state.data[id],
-            content
-          }
-        }
-      };
+      state.data[id].content = content;
+      return;
 
     case ActionType.DELETE_CELL:
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload);
       return state;
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) return state;
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
       return state;
+
     case ActionType.INSERT_CELL_BEFORE:
+      const cell: Cell = {
+        content: '',
+        type: action.payload.type,
+        id: randomId()
+      };
+
+      state.data[cell.id] = cell;
+
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+
+      if (foundIndex < 0) state.order.push(cell.id);
+      else state.order.splice(foundIndex, 0, cell.id);
       return state;
     default:
       return state;
   }
+});
+
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 5);
 };
 
 export default reducer;
